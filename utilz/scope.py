4ec7d5279cf58e4c7e924bede974241f02f8072f
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-09-08 01:52:54 +0300

    Fix `IScope` member access method signature

--- utilz/scope.py
+++ utilz/scope.py
@@ -1,9 +1,10 @@
 from typeclass import Typeclass, typeclass_api
 
+from miniz.core import ObjectProtocol
 from utilz.code_generation.core import CodeGenerationResult
 
 
 class IScope(Typeclass):
     @typeclass_api
-    def get_member(self, item: CodeGenerationResult, name: str) -> CodeGenerationResult:
+    def get_member(self, item: CodeGenerationResult, name: str) -> CodeGenerationResult | ObjectProtocol:
         raise NotImplementedError

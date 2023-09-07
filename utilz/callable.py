a7596f442e8f2f9d2171d27371e0271e64e319b1
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-09-08 01:52:36 +0300

    Fix `ICallable` call methods signatures

--- utilz/callable.py
+++ utilz/callable.py
@@ -1,14 +1,15 @@
 from typeclass import typeclass_api, Typeclass
 
+from miniz.core import ObjectProtocol
 from miniz.interfaces.overloading import Argument
 from utilz.code_generation.core import CodeGenerationResult
 
 
 class ICallable(Typeclass):
     @typeclass_api
-    def curvy_call(self, compiler, item: CodeGenerationResult, args: list[Argument], kwargs: list[tuple[str, Argument]]) -> CodeGenerationResult:
+    def curvy_call(self, compiler, item: CodeGenerationResult, args: list[Argument], kwargs: list[tuple[str, Argument]]) -> CodeGenerationResult | ObjectProtocol:
         raise NotImplementedError
 
     @typeclass_api
-    def square_call(self, compiler, item: CodeGenerationResult, args: list[Argument], kwargs: list[tuple[str, Argument]]) -> CodeGenerationResult:
+    def square_call(self, compiler, item: CodeGenerationResult, args: list[Argument], kwargs: list[tuple[str, Argument]]) -> CodeGenerationResult | ObjectProtocol:
         raise NotImplementedError
